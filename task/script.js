02ffd5e66cb2fa8be86c73fc4c12d606cdada82d
Oto48 <66161545+Oto48@users.noreply.github.com>
2022-11-30 20:12:26 +0400

    Update script.js

--- task/script.js
+++ task/script.js
@@ -3,24 +3,21 @@ function example (str, search){
     let searchLower = search.toLowerCase()
     let count = 0;
     for(let i = 0; i < strLower.length; i++){
+        let text = '';
         let len = 0;
-        let text = strLower[i];
-        for(let j = 1; j < searchLower.length; j++){
-            text += strLower[i + j];
-        }
-
-        for(let k = 0; k < text.length; k++){
-            for(let i = 0; i < searchLower.length; i++){
-                if(searchLower[i] == text[k]){
+        for(let j = 0; j < searchLower.length; j++){
+            for(let k = 0; k < searchLower.length; k++){
+                if(searchLower[k] == strLower[i + j]){
+                    text += strLower[i + j];
                     len++;
                     break;
                 }
             }
-        }
-
-        if(text.length == len){
-            count++;
-            i += searchLower.length - 1;
+            
+            if(len == searchLower.length){
+                count++;
+                i += searchLower.length - 1;
+            }
         }
     }
     console.log(count);
